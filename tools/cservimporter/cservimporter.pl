5ff3d00396ed4064ed96da6a2303ccf9a380daa4
Sjors Gielen <sjors@sjorsgielen.nl>
2014-03-05 02:58:41 +0100

    Import invoices from CServ into liminfra.

--- tools/cservimporter/cservimporter.pl
+++ tools/cservimporter/cservimporter.pl
@@ -49,6 +49,8 @@ sub import_from_cserv_mongo {
 	try {
 		$dbh->begin_work;
 		$dbh->do("LOCK TABLE account IN ACCESS EXCLUSIVE MODE;");
+		$dbh->do("LOCK TABLE invoice IN ACCESS EXCLUSIVE MODE;");
+		$dbh->do("LOCK TABLE invoice_itemline IN ACCESS EXCLUSIVE MODE;");
 		$dbh->do("LOCK TABLE phonenumber IN ACCESS EXCLUSIVE MODE;");
 		$dbh->do("LOCK TABLE sim IN ACCESS EXCLUSIVE MODE;");
 		$dbh->do("LOCK TABLE speakupAccount IN ACCESS EXCLUSIVE MODE;");
@@ -204,6 +206,62 @@ sub import_sims {
 	}
 }
 
+=head3 import_invoices($lim, $cservdb, $dbh, $accounts_map)
+
+Import the SIMs from Mongo to Liminfra. The given database is a
+MongoDB::Database pointing at CServ's database; the given dbh is a DBI handle
+pointing at liminfra's database. The dbh must be in transaction state and
+should have an exclusive lock on the 'invoice' and 'invoice_itemline' tables.
+The accounts_map is a hashref containing CServ account ID's as keys and
+liminfra account ID's as values.
+
+=cut
+
+sub import_invoices {
+	my ($lim, $cservdb, $dbh, $accounts_map) = @_;
+	my $collection = $cservdb->get_collection("invoices");
+
+	verify_table_empty($dbh, "invoice");
+	verify_table_empty($dbh, "invoice_itemline");
+
+	my $cursor = $collection->find();
+	while(my $invoice = $cursor->next) {
+		my $id = $invoice->{'_id'};
+		my $mongo_account_id = $invoice->{'accountId'};
+		my $account_id = $accounts_map->{$mongo_account_id};
+
+		my $date = $invoice->{'creationDate'};
+		my $invoice_date = $date->ymd;
+		my $creation_time = $date->iso8601();
+
+		my $sth = $dbh->prepare("INSERT INTO invoice (id, account_id, date,
+		   creation_time, rounded_without_taxes, rounded_with_taxes) VALUES
+		   (?, ?, ?, ?, ?::numeric/10000, ?::numeric/10000);");
+		$sth->execute($id, $account_id, $invoice_date, $creation_time,
+			$invoice->{'totalWithoutTaxes'}, $invoice->{'totalWithTaxes'});
+
+		$sth = $dbh->prepare("INSERT INTO invoice_itemline (invoice_id, description,
+		   type, taxrate, item_price, item_count, rounded_total,
+		   number_of_calls, number_of_seconds, price_per_call,
+		   price_per_minute) VALUES (?, ?, ?, ?, ?::numeric/10000, ?, ?::numeric/10000,
+		   ?, ?, ?::numeric/10000, ?::numeric/10000);");
+		foreach my $line (@{$invoice->{'itemLines'}}) {
+			my $description = $line->{'description'};
+			if($line->{'multilineDescription'} && @{$line->{'multilineDescription'}}) {
+				$description = join "\n", @{$line->{'multilineDescription'}};
+			}
+			$sth->execute($id, $description, uc($line->{'type'}),
+				map {$line->{$_}} qw(taxRate itemPrice
+				itemCount totalPrice numberOfCalls
+				numberOfSeconds pricePerCall pricePerMinute));
+		}
+		foreach my $line (@{$invoice->{'taxLines'}}) {
+			$sth->execute($id, "Tax", "TAX", $line->{'taxRate'}, $line->{'baseAmount'},
+				1, $line->{'taxAmount'}, undef, undef, undef, undef);
+		}
+	}
+}
+
 =head3 import_cdrs($database)
 
 Import the CDRs from Mongo to Liminfra.
@@ -211,16 +269,6 @@ Import the CDRs from Mongo to Liminfra.
 =cut
 
 sub import_cdrs {
-	my ($database) = @_;
-}
-
-=head3 import_invoices($database)
-
-Import the invoices from Mongo to Liminfra.
-
-=cut
-
-sub import_invoices {
 	my ($database) = @_;
 }
 
