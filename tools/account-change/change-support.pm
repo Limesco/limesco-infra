6940a30816f453cc0a01bb9a02bf6dad821d0402
Sjors Gielen <sjors@sjorsgielen.nl>
2014-06-03 00:24:29 +0200

    account-change: Rename all mentions of 'account' to 'object' in change-support.

--- tools/account-change/change-support.pm
+++ tools/account-change/change-support.pm
@@ -31,20 +31,21 @@ sub get_object {
 	return $object;
 }
 
-=head3 create_object($lim, $object_info, $account, [$date])
+=head3 create_object($lim, $object_info, $object, [$date])
 
-Create an account. $date is the optional starting date of the account; if not
-given, 'today' is assumed. $account must contain all required fields (e.g.
-first_name and street_address), may contain zero or more of the optional
-fields (e.g. company_name), and may not contain any other fields.
+Create an object. $date is the optional starting date of the object; if not
+given, 'today' is assumed. $object must contain all required fields from the
+$object_info (such as first_name and street_address for an account), may
+contain zero or more of the optional fields (such as company_name), and may not
+contain any other fields.
 
-This method returns the newly created account, or throws an exception if
+This method returns the newly created object, or throws an exception if
 something failed.
 
 =cut
 
 sub create_object {
-	my ($lim, $object_info, $account, $date) = @_;
+	my ($lim, $object_info, $object, $date) = @_;
 	$date ||= 'today';
 	my $dbh = $lim->get_database_handle();
 
@@ -52,22 +53,22 @@ sub create_object {
 	my @db_values;
 
 	foreach(@{$object_info->{'required_fields'}}) {
-		if(!exists($account->{$_}) || length($account->{$_}) == 0) {
-			die "Required account field $_ is missing in create_account";
+		if(!exists($object->{$_}) || length($object->{$_}) == 0) {
+			die "Required object field $_ is missing in create_object";
 		}
 		push @db_fields, $_;
-		push @db_values, delete $account->{$_};
+		push @db_values, delete $object->{$_};
 	}
 
 	foreach(@{$object_info->{'optional_fields'}}) {
-		if(exists($account->{$_})) {
+		if(exists($object->{$_})) {
 			push @db_fields, $_;
-			push @db_values, delete $account->{$_};
+			push @db_values, delete $object->{$_};
 		}
 	}
 
-	foreach(keys %$account) {
-		die "Unknown account field $_ in create_account\n";
+	foreach(keys %$object) {
+		die "Unknown object field $_ in create_object\n";
 	}
 
 	unshift @db_fields, "period";
@@ -83,25 +84,25 @@ sub create_object {
 	my $sth = $dbh->prepare($query);
 	$sth->execute(@db_values);
 
-	my $account_id = $dbh->last_insert_id(undef, undef, undef, undef, {sequence => $primary_key_seq});
-	return get_object($lim, $object_info, $account_id, $date);
+	my $object_id = $dbh->last_insert_id(undef, undef, undef, undef, {sequence => $primary_key_seq});
+	return get_object($lim, $object_info, $object_id, $date);
 }
 
-=head3 update_object($lim, $object_info, $account_id, $changes, [$date])
+=head3 update_object($lim, $object_info, $object_id, $changes, [$date])
 
-Update an account. $date is the optional date of the changes; if not given,
-'today' is assumed. $account_id is the ID of the account to change, $changes
-is a hashref of wanted changes. If any of the given changes is impossible, the
-full update is aborted and an exception is thrown. $changes must only contain
-fields that are either required or optional for an account.
+Update an object. $date is the optional date of the changes; if not given,
+'today' is assumed. $object_id is the primary key value of the object to
+change, $changes is a hashref of wanted changes. If any of the given changes is
+impossible, the full update is aborted and an exception is thrown. $changes
+must only contain fields that are either required or optional for an object.
 
-This method returns the updated account, or throws an exception if something
+This method returns the updated object, or throws an exception if something
 failed.
 
 =cut
 
 sub update_object {
-	my ($lim, $object_info, $account_id, $changes, $date) = @_;
+	my ($lim, $object_info, $object_id, $changes, $date) = @_;
 	$date ||= 'today';
 	my $dbh = $lim->get_database_handle();
 
@@ -114,23 +115,23 @@ sub update_object {
 		$dbh->do("LOCK TABLE $table_name;");
 
 		my $sth = $dbh->prepare("SELECT *, lower(period) AS old_date, ?::date AS new_date FROM $table_name WHERE $primary_key=? AND upper(period) IS NULL AND period @> ?::date");
-		$sth->execute($date, $account_id, $date);
-		my $old_account = $sth->fetchrow_hashref;
-		if(!$old_account) {
-			die "Cannout change account $account_id at date $date, doesn't exist or it is already historical";
+		$sth->execute($date, $object_id, $date);
+		my $old_object = $sth->fetchrow_hashref;
+		if(!$old_object) {
+			die "Cannout change object $object_id at date $date, doesn't exist or it is already historical";
 		}
 
 		# If the new date overwrites the last period, delete the row, otherwise update it
 		my $changed_rows;
-		if($old_account->{'old_date'} eq $old_account->{'new_date'}) {
+		if($old_object->{'old_date'} eq $old_object->{'new_date'}) {
 			my $sth = $dbh->prepare("DELETE FROM $table_name WHERE $primary_key=? AND period=?");
-			$changed_rows = $sth->execute($old_account->{'id'}, $old_account->{'period'});
+			$changed_rows = $sth->execute($old_object->{'id'}, $old_object->{'period'});
 		} else {
 			my $sth = $dbh->prepare("UPDATE $table_name SET period=daterange(lower(period), ?) WHERE $primary_key=? AND period=?");
-			$changed_rows = $sth->execute($date, $old_account->{id}, $old_account->{period});
+			$changed_rows = $sth->execute($date, $old_object->{id}, $old_object->{period});
 		}
 		if(!$changed_rows) {
-			die "Failed to change account $account_id, even though it existed";
+			die "Failed to change object $object_id, even though it existed";
 		}
 
 		my @db_fields;
@@ -139,7 +140,7 @@ sub update_object {
 		foreach(@{$object_info->{'required_fields'}}) {
 			push @db_fields, $_;
 			if(!exists($changes->{$_}) || length($changes->{$_}) == 0) {
-				push @db_values, $old_account->{$_};
+				push @db_values, $old_object->{$_};
 			} else {
 				push @db_values, delete $changes->{$_};
 			}
@@ -150,19 +151,19 @@ sub update_object {
 			if(exists($changes->{$_})) {
 				push @db_values, delete $changes->{$_};
 			} else {
-				push @db_values, $old_account->{$_};
+				push @db_values, $old_object->{$_};
 			}
 		}
 
 		foreach(keys %$changes) {
-			die "Unknown account field $_ in update_account\n";
+			die "Unknown object field $_ in update_object\n";
 		}
 
 		unshift @db_fields, "period";
 		unshift @db_values, '['.$date.',)';
 
 		unshift @db_fields, $primary_key;
-		unshift @db_values, $account_id;
+		unshift @db_values, $object_id;
 
 		my $query = "INSERT INTO $table_name (" . join(", ", @db_fields) . ")";
 		$query .= " VALUES (" . join (", ", (('?') x @db_fields)) . ")";
@@ -170,22 +171,22 @@ sub update_object {
 		$sth = $dbh->prepare($query);
 		$sth->execute(@db_values);
 		$dbh->commit;
-		return get_object($lim, $object_info, $account_id, $date);
+		return get_object($lim, $object_info, $object_id, $date);
 	} catch {
 		$dbh->rollback;
 		die $_;
 	};
 }
 
-=head3 delete_object($lim, $object_info, $account_id, [$date])
+=head3 delete_object($lim, $object_info, $object_id, [$date])
 
-Delete an account. $date is the optional date of deletion; if not given,
+Delete an object. $date is the optional date of deletion; if not given,
 'today' is assumed.
 
 =cut
 
 sub delete_object {
-	my ($lim, $object_info, $account_id, $date) = @_;
+	my ($lim, $object_info, $object_id, $date) = @_;
 	$date ||= 'today';
 	my $dbh = $lim->get_database_handle();
 
@@ -197,24 +198,24 @@ sub delete_object {
 
 		$dbh->do("LOCK TABLE $table_name;");
 		my $sth = $dbh->prepare("SELECT $primary_key, period, lower(period) AS old_date, ?::date AS new_date FROM $table_name WHERE $primary_key=? AND upper(period) IS NULL AND period @> ?::date");
-		$sth->execute($date, $account_id, $date);
-		my $old_account = $sth->fetchrow_hashref;
-		if(!$old_account) {
-			die "Cannout change account $account_id at date $date, doesn't exist or it is already historical";
+		$sth->execute($date, $object_id, $date);
+		my $old_object = $sth->fetchrow_hashref;
+		if(!$old_object) {
+			die "Cannout change object $object_id at date $date, doesn't exist or it is already historical";
 		}
 
 		# If the new date overwrites the last period, delete the row, otherwise update it
 		my $changed_rows;
-		if($old_account->{'old_date'} eq $old_account->{'new_date'}) {
+		if($old_object->{'old_date'} eq $old_object->{'new_date'}) {
 			my $sth = $dbh->prepare("DELETE FROM $table_name WHERE $primary_key=? AND period=?");
-			$changed_rows = $sth->execute($old_account->{'id'}, $old_account->{'period'});
+			$changed_rows = $sth->execute($old_object->{'id'}, $old_object->{'period'});
 		} else {
 			my $sth = $dbh->prepare("UPDATE $table_name SET period=daterange(lower(period), ?) WHERE $primary_key=? AND period=?");
-			$changed_rows = $sth->execute($date, $old_account->{id}, $old_account->{period});
+			$changed_rows = $sth->execute($date, $old_object->{id}, $old_object->{period});
 		}
 
 		if(!$changed_rows) {
-			die "Failed to delete account $account_id, even though it existed";
+			die "Failed to delete object $object_id, even though it existed";
 		}
 
 		$dbh->commit;
